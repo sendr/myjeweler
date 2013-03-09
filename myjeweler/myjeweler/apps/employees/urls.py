2cadb7704a21f1594038639c26cf54e0cf34e6b3
sendr <sendr84@gmail.com>
2013-03-09 22:43:45 +0200

    Added page create

--- myjeweler/myjeweler/apps/employees/urls.py
+++ myjeweler/myjeweler/apps/employees/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('myjeweler.apps.employees.views',
     url(r'^$', 'employees_group_view', name ='employees_group_view'),
     url(r'^employees_group_list/(?P<id>\d+)/$', 'employees_group_list', name = 'employees_group_list'),
     url(r'^(?P<id>\d+)/$', 'employee_one', name = 'employee_one'),
-    url(r'^employees_list/$', 'employees_list', name ='employees_group_view'),
+    url(r'^employees_list/$', 'employees_list', name ='employees_list'),
 )
--- myjeweler/myjeweler/apps/employees/views.py
+++ myjeweler/myjeweler/apps/employees/views.py
@@ -1,6 +1,7 @@
-
-from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, get_object_or_404, redirect
 from myjeweler.apps.employees.models import Group, Employee
+from django.forms import ModelForm
 
 
 
@@ -27,4 +28,25 @@ def	employees_list(request):
 	items = Employee.objects.all()
 	return render(request, 'employees_list.html',
 				{'items': items})
-	
+
+class EmployeeForm(ModelForm):
+
+	class Meta:
+		model = Employee
+
+def create_view(request, id=None):
+	if id is not None:
+		employee = get_object_or_404(Employee,id=id)
+	else:
+		employee = None
+
+	if request.GET:
+		form = EmployeeForm(request.GET, instance=employee)
+		if form.is_valid():
+			employee = form.save()
+			return redirect(reverse('employee_one', args=[employee.id]))
+	else:
+		form = EmployeeForm()
+
+	return render(request,'employee_create.html',{
+					'form': form, 'employee': employee})
